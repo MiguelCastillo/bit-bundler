6ba20c65ab47062a6764bfae428dacaf1989824d
Miguel Castillo <manchagnu@gmail.com>
2016-01-08 21:51:36 -0500

    Removed all packages as they have been pushed to their own repos

--- packages/bit-bundler-css/loadstyle.js
+++
@@ -1,9 +0,0 @@
-function attachToDOM(source) {
-  var head = document.getElementsByTagName('head')[0];
-  var style = document.createElement('style');
-  style.setAttribute('type', 'text/css');
-  style.innerHTML = source;
-  head.appendChild(style);
-}
-
-module.exports = attachToDOM;
