d5b0ca41ebabc17f8e60b388662ebc40e4bc77eb
Miguel Castillo <manchagnu@gmail.com>
2016-07-01 02:22:32 -0400

    Added initial unit test setup

--- test/spec/index.js
+++ test/spec/index.js
@@ -1,7 +1,35 @@
-import { expect } from 'chai';
+import { expect } from "chai";
+import Bundler from "../../src/index";
 
-describe('Test suite', () => {
-  it('pandaBear.eats returns `Bamboo and more`', () => {
-    expect('Bamboo and more').to.equal('Bamboo and more');
+describe("Bundler test suite", function() {
+  var act, bundler, config;
+
+  beforeEach(function() {
+    act = () => bundler = new Bundler(config);
+  });
+
+  describe("When creating a bundler with no configuration", function() {
+    beforeEach(function() {
+      config = null;
+      act();
+    });
+
+    it("then the bundler is an instance of Bundler", function() {
+      expect(bundler).to.be.an.instanceof(Bundler);
+    });
+
+    describe("and bundling a file with just one function", function() {
+      var result;
+
+      beforeEach(function() {
+        return bundler.bundle("test/sample/basic").then(function(ctx) {
+          result = ctx.bundle.result.replace(/\n/g, "");
+        });
+      });
+
+      it("then result is the string bundle", function() {
+        expect(result).to.be.equal(`require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){function X() {}},{}]},{},[1])//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm5vZGVfbW9kdWxlcy9icm93c2VyLXBhY2svX3ByZWx1ZGUuanMiLCJ0ZXN0L3NhbXBsZS9iYXNpYy5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtBQ0FBIiwiZmlsZSI6ImdlbmVyYXRlZC5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzQ29udGVudCI6WyIoZnVuY3Rpb24gZSh0LG4scil7ZnVuY3Rpb24gcyhvLHUpe2lmKCFuW29dKXtpZighdFtvXSl7dmFyIGE9dHlwZW9mIHJlcXVpcmU9PVwiZnVuY3Rpb25cIiYmcmVxdWlyZTtpZighdSYmYSlyZXR1cm4gYShvLCEwKTtpZihpKXJldHVybiBpKG8sITApO3ZhciBmPW5ldyBFcnJvcihcIkNhbm5vdCBmaW5kIG1vZHVsZSAnXCIrbytcIidcIik7dGhyb3cgZi5jb2RlPVwiTU9EVUxFX05PVF9GT1VORFwiLGZ9dmFyIGw9bltvXT17ZXhwb3J0czp7fX07dFtvXVswXS5jYWxsKGwuZXhwb3J0cyxmdW5jdGlvbihlKXt2YXIgbj10W29dWzFdW2VdO3JldHVybiBzKG4/bjplKX0sbCxsLmV4cG9ydHMsZSx0LG4scil9cmV0dXJuIG5bb10uZXhwb3J0c312YXIgaT10eXBlb2YgcmVxdWlyZT09XCJmdW5jdGlvblwiJiZyZXF1aXJlO2Zvcih2YXIgbz0wO288ci5sZW5ndGg7bysrKXMocltvXSk7cmV0dXJuIHN9KSIsImZ1bmN0aW9uIFgoKSB7fSJdfQ==`);
+      });
+    });
   });
 });
