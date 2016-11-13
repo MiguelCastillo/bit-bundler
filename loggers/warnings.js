8346be6b1cd2ad1aa649dfc18c0b73cecdd0e457
Miguel Castillo <mcastillo@symphono.com>
2016-11-12 23:15:53 -0500

    changed buildstats logger for cleaner reports of errors and enabled the animation by default

--- loggers/warnings.js
+++ loggers/warnings.js
@@ -2,14 +2,15 @@ var chalk = require("chalk");
 var es = require("event-stream");
 var messageBuilder = require("./messageBuilder");
 
-function warningsStreamFactory() {
+function warningsStreamFactory(options) {
   return es.map(function(chunk, callback) {
-    logWarningsAndErrors(chunk);
+    logWarningsAndErrors(chunk, options);
     callback(null, chunk);
   });
 }
 
-function logWarningsAndErrors(chunk) {
+function logWarningsAndErrors(chunk, options) {
+  options = options || {};
   var color = (
     chunk.level === 2 ? chalk.yellow :
     chunk.level === 3 ? chalk.red : null
@@ -19,15 +20,20 @@ function logWarningsAndErrors(chunk) {
     var msgs = messageBuilder(chunk);
 
     if (msgs.length) {
-      if (chunk.name) {
-        console.log(color(">> [" + chunk.name + "]"));
+      if (chunk.name && options.showName !== false) {
+        process.stderr.write(color(">> [" + chunk.name + "]") + "\n");
       }
 
-      msgs.forEach(function(d) { console.log("  " + d); });
-      console.log("");
+      msgs.forEach(function(d) { process.stderr.write("  " + d + "\n"); });
+      process.stderr.write("\n");
     }
   }
 }
 
+function hasWarningsOrErrors(chunk) {
+  return chunk.level === 2 || chunk.level === 3;
+}
+
+warningsStreamFactory.hasWarningsOrErrors = hasWarningsOrErrors;
 warningsStreamFactory.logWarningsAndErrors = logWarningsAndErrors;
 module.exports = warningsStreamFactory;
