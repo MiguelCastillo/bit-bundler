e7d5762a98d3baebfbdb8d66b7c6b5efb5d16dc5
Miguel Castillo <mcastillo@symphono.com>
2017-05-21 12:07:03 -0400

    adding initial wiring to run the loader in a separate process

--- src/loader.js
+++ src/loader.js
@@ -1,73 +1,118 @@
-var Bitloader = require("bit-loader");
+var path = require("path");
 var utils = require("belty");
-var resolvePath = require("bit-bundler-utils/resolvePath");
-var readFile = require("bit-bundler-utils/readFile");
-var logger = require("./logger").create("bundler/loader");
-
+var childProcess = require('child_process');
 
 function Loader(options) {
-  Bitloader.call(this, utils.extend({
-    resolve: configureResolve(options),
-    fetch: configureFetch(options)
-  }, options));
+  this.options = utils.assign({}, options);
+  this.cache = {};
+  this.pending = {};
+  this.id = 0;
+
+  var loaderProc = childProcess.fork(path.resolve(__dirname, "./loaderProc.js"), {
+    cwd: process.cwd(),
+    env: process.env,
+    silent: true
+  });
+
+  this.loaderProc = loaderProc;
+
+  loaderProc.stdout.pipe(process.stdout);
+  loaderProc.stderr.pipe(process.stderr);
+  registerLoaderProcHandlers(this);
 }
 
+Loader.prototype.init = function() {
+  return init(this, {
+    type: "init",
+    data: this.options
+  });
+};
 
-Loader.prototype = Object.create(Bitloader.prototype);
-Loader.prototype.constructor = Loader;
-
-
-function configureResolve(options) {
-  var resolver = resolvePath.configure({baseUrl: options.baseUrl});
-
-  return function resolveName(meta) {
-    function handleError(err) {
-      var message = err && err.message;
-
-      if (message && message.indexOf("Cannot find module") === 0 && options.ignoreNotFound) {
-        logger.warn("Module not found. Skipping it.", moduleNotFoundError(meta));
-      }
-      else {
-        logger.error(moduleNotResolvedError(meta), err);
-        throw err;
-      }
-
-      return {
-        id: "@notfound",
-        path: ""
-      };
+Loader.prototype.fetch = function(name, referrer) {
+  var loader = this;
+  return send(this, {
+    type: "fetch",
+    data: {
+      name: name,
+      referrer: referrer
     }
+  })
+  .then(function(result) {
+    loader.cache = result.cache;
+    return result.module;
+  });
+};
 
-    return resolver(meta).then(utils.identity, handleError);
-  };
+Loader.prototype.getModule = function(id) {
+  return this.cache[id];
+};
+
+Loader.prototype.deleteModule = function(mod) {
+  delete this.cache[mod.id];
+  send(this, {
+    type: "delete",
+    id: mod.id
+  });
+};
+
+Loader.prototype.getCache = function() {
+  return this.cache;
+};
+
+
+function init(loader, message) {
+  if (loader._init) {
+    return loader._init.deferred;
+  }
+
+  var id = loader.id++;
+  var deferred = new Promise(function(resolve, reject) {
+    loader._init = { id: id };
+    loader._init.resolve = resolve;
+    loader._init.reject = reject;
+    loader._init.deferred = deferred;
+    loader.loaderProc.send(utils.assign({ id: id }, message));
+  });
+
+  return deferred;
 }
 
+function send(loader, message) {
+  var id = loader.id++;
+  var deferred = new Promise(function(resolve, reject) {
+    loader.pending[id] = {};
+    loader.pending[id].resolve = resolve;
+    loader.pending[id].reject = reject;
+    loader.pending[id].deferred = deferred;
 
-function configureFetch(options) {
-  return function fetchModule(meta) {
-    function handleError(err) {
-      if (err && err.code === "ENOENT" && options.ignoreNotFound) {
-        logger.warn("Module not found. Skipping it.", moduleNotFoundError(meta));
-        return Promise.resolve({ source: "module.exports = require('" + meta.name + "')" });
+    loader.init().then(function() {
+      return loader.loaderProc.send(utils.assign({ id: id }, message));
+    });
+  });
+
+  return deferred;
+}
+
+function registerLoaderProcHandlers(loader) {
+  loader.loaderProc
+    .on("error", function(error) {
+      console.error(error);
+    })
+    .on("message", function(message) {
+      if (loader.pending.hasOwnProperty(message.id)) {
+        handleResult(message, loader.pending[message.id]);
+        delete loader.pending[message.id];
       }
-
-      logger.error(moduleNotLoadedError(meta), err);
-      throw err;
-    }
-
-    return meta.id === "@notfound" && options.ignoreNotFound ?
-      Promise.resolve({ source: "" }) :
-      readFile(meta).then(utils.identity, handleError);
-  };
+      else if (loader._init.id === message.id) {
+        handleResult(message, loader._init);
+      }
+    });
 }
 
-
-var moduleNotFoundError = buildError.bind(null, "Unable to find module");
-var moduleNotLoadedError = buildError.bind(null, "Unable to load module");
-var moduleNotResolvedError = buildError.bind(null, "Unable to resolve module");
-
-function buildError(title, meta) {
-  return title + " '" + meta.name + "'." + (meta.referrer ? " Referrer " + JSON.stringify(meta.referrer.path) : "");
+function handleResult(message, pending) {
+  message.error ?
+    pending.reject(message.error) :
+    pending.resolve(message.data);
 }
 
 module.exports = Loader;
---
+++ src/loaderProc.js
@@ -0,0 +1,114 @@
+var Bitloader = require("bit-loader");
+var utils = require("belty");
+var resolvePath = require("bit-bundler-utils/resolvePath");
+var readFile = require("bit-bundler-utils/readFile");
+var logger = require("./logger").create("bundler/loader");
+
+
+function Loader(options) {
+  Bitloader.call(this);
+  registerMessageHandlers(this);
+}
+
+
+Loader.prototype = Object.create(Bitloader.prototype);
+Loader.prototype.constructor = Loader;
+
+
+function registerMessageHandlers(loader) {
+  process.on("message", function(message) {
+    switch(message.type) {
+      case "init":
+        var options = message.data;
+
+        loader = loader
+          .configure(utils.extend({
+            resolve: configureResolve(options),
+            fetch: configureFetch(options)
+          }, options));
+
+        process.send({ id: message.id });
+        break;
+      case "fetch":
+        loader
+          .fetch(message.data.name)
+          .then(function(module) {
+            process.send({
+              id: message.id,
+              data: {
+                module: module,
+                cache: loader.cache
+              }
+            });
+          })
+          .catch(function(error) {
+            process.send({
+              id: message.id,
+              error: error
+            });
+          });
+        break;
+      case "delete":
+        loader.deleteModule(module.data.id);
+        process.send({ id: message.id });
+        break;
+    }
+  });
+}
+
+
+function configureResolve(options) {
+  var resolver = resolvePath.configure({baseUrl: options.baseUrl});
+
+  return function resolveName(meta) {
+    function handleError(err) {
+      var message = err && err.message;
+
+      if (message && message.indexOf("Cannot find module") === 0 && options.ignoreNotFound) {
+        logger.warn("Module not found. Skipping it.", moduleNotFoundError(meta));
+      }
+      else {
+        logger.error(moduleNotResolvedError(meta), err);
+        throw err;
+      }
+
+      return {
+        id: "@notfound",
+        path: ""
+      };
+    }
+
+    return resolver(meta).then(utils.identity, handleError);
+  };
+}
+
+
+function configureFetch(options) {
+  return function fetchModule(meta) {
+    function handleError(err) {
+      if (err && err.code === "ENOENT" && options.ignoreNotFound) {
+        logger.warn("Module not found. Skipping it.", moduleNotFoundError(meta));
+        return Promise.resolve({ source: "module.exports = require('" + meta.name + "')" });
+      }
+
+      logger.error(moduleNotLoadedError(meta), err);
+      throw err;
+    }
+
+    return meta.id === "@notfound" && options.ignoreNotFound ?
+      Promise.resolve({ source: "" }) :
+      readFile(meta).then(utils.identity, handleError);
+  };
+}
+
+
+var moduleNotFoundError = buildError.bind(null, "Unable to find module");
+var moduleNotLoadedError = buildError.bind(null, "Unable to load module");
+var moduleNotResolvedError = buildError.bind(null, "Unable to resolve module");
+
+function buildError(title, meta) {
+  return title + " '" + meta.name + "'." + (meta.referrer ? " Referrer " + JSON.stringify(meta.referrer.path) : "");
+}
+
+module.exports = Loader;
+module.exports.instance = new Loader();
