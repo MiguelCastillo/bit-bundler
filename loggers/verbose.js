5921a185391d02a8576aab264d5619698643d98f
Miguel Castillo <mcastillo@symphono.com>
2017-06-27 18:03:19 -0400

    removed the chunk level and instead rely on the levelFilter logger stream
    Also - fixed issues with the logger just not correctly writing to the
    stream

--- loggers/verbose.js
+++ loggers/verbose.js
@@ -1,22 +1,23 @@
 var chalk = require("chalk");
 var es = require("event-stream");
 var messageBuilder = require("./messageBuilder");
+var logger = require("../src/logger");
 
-function verboseStreamFactory(level) {
+function verboseStreamFactory() {
   return es.through(function(chunk) {
     var msgs = messageBuilder(chunk);
 
     var color = (
-      chunk.level === 1 ? chalk.green :
-      chunk.level === 2 ? chalk.yellow :
-      chunk.level === 3 ? chalk.red :
+      chunk.level === logger.levels.info ? chalk.green :
+      chunk.level === logger.levels.warn ? chalk.yellow :
+      chunk.level === logger.levels.error ? chalk.red :
       chalk.blue
     );
 
-    if (msgs.length && chunk.level >= level) {
-      process.stderr(color(">> [" + chunk.name + "]"));
-      msgs.forEach(function(d) { process.stderr("  " + d + "\n"); });
-      process.stderr("\n");
+    if (msgs.length) {
+      process.stderr.write(color(">> [" + chunk.name + "]"));
+      msgs.forEach(function(d) { process.stderr.write("  " + d + "\n"); });
+      process.stderr.write("\n");
     }
 
     this.emit("data", chunk);
