fd3f5c4388e8786cbac1903ce7b168f90690b8bf
Miguel Castillo <mcastillo@symphono.com>
2017-06-27 18:25:30 -0400

    removed babel and splitter from watch example. also cleaned up the streams used for logging the watch and build messages

--- examples/watch/index.js
+++ examples/watch/index.js
@@ -1,20 +1,15 @@
 var Bitbundler = require("bit-bundler");
-var splitBundle = require("bit-bundler-splitter");
+var throughStream = require("bit-bundler/loggers/through");
 var buildstatsLogger = require("bit-bundler/loggers/buildstats");
 var watchLogger = require("bit-bundler/loggers/watch");
 
-var logger = watchLogger();
-logger.pipe(buildstatsLogger());
+var logger = throughStream();
+logger.pipe(watchLogger()).pipe(buildstatsLogger());
 
 var bitbundler = new Bitbundler({
   log: logger,
   loader: [
-    "bit-loader-js",
-    "bit-loader-babel"
-  ],
-  bundler: [
-    splitBundle("renderer", { match: { path: /src\/renderer/ }, dest: "dest/renderer.js" }),
-    splitBundle("other.js", { match: { fileName: "other.js" }, dest: "dest/other.js" })
+    "bit-loader-js"
   ],
 
   // Enable watching. You can alternatively pass
