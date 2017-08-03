f79bb51abc4aeb9b254142b5e2cdccda5759a3ae
Miguel Castillo <mcastillo@symphono.com>
2017-08-03 12:48:05 -0400

    renamed `.bitbundlerrc` not include the `rc` postfix.

--- bin/cli.js
+++ bin/cli.js
@@ -7,7 +7,7 @@ var argv = require("subarg")(process.argv.slice(2));
 var options = camelKeys(argv);
 
 try {
-  options.config = path.join(process.cwd(), typeof options.config === "string" ? options.config : ".bitbundlerrc");
+  options.config = path.join(process.cwd(), typeof options.config === "string" ? options.config : ".bitbundler");
   options = Object.assign({}, camelKeys(require(options.config)), options);
 }
 catch(ex) {
