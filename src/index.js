9bf51bdb9da9131b0fa768708e6286e17ed48488
Miguel Castillo <mcastillo@symphono.com>
2017-08-04 14:51:25 -0400

    migrated index.js to use classes and lambdas

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var defaultOptions = require("./defaultOptions");
 var utils = require("belty");
 var File = require("src-dest");
@@ -16,124 +18,111 @@ var buildstats = require("../loggers/buildstats");
 
 var logger = loggerFactory.create("bundler/build");
 
+class Bitbundler extends EventEmitter {
+  constructor(options) {
+    super();
 
-function Bitbundler(options) {
-  if (!(this instanceof Bitbundler)) {
-    return new Bitbundler(options);
-  }
+    this.context = null;
+    this.options = Object.assign({}, defaultOptions, options);
 
-  this.context = null;
-  this.options = Object.assign({}, defaultOptions, options);
-
-  // ignoreNotFound is deprecated...
-  if (this.options.hasOwnProperty("ignoreNotFound")) {
-    this.options.stubNotFound = this.options.ignoreNotFound;
-    delete this.options.ignoreNotFound;
-    deprecate("ignoreNotFound is deprecated", "Please use stubNotFound");
-  }
-
-  configureNotifications(this, this.options.notifications);
-  configureLogger(this, this.options.log, loggerFactory);
-}
-
-Bitbundler.prototype = Object.create(EventEmitter.prototype);
-Bitbundler.prototype.constructor = Bitbundler;
-
-
-Bitbundler.prototype.bundle = function(files) {
-  var file = new File(files);
-  var bitbundler = this;
-  bitbundler.context = bitbundler._createContext(file, bitbundler.options);
-  logger.log("build-init");
-
-  return bitbundler.update(files).then(function(context) {
-    if (bitbundler.options.watch) {
-      watch(bitbundler, bitbundler.options.watch);
-
-      if (bitbundler.options.multiprocess) {
-        context.loader.pool.size(1);
-      }
-    }
-    else if (bitbundler.options.multiprocess) {
-      context.loader.pool.stop();
+    // ignoreNotFound is deprecated...
+    if (this.options.hasOwnProperty("ignoreNotFound")) {
+      this.options.stubNotFound = this.options.ignoreNotFound;
+      delete this.options.ignoreNotFound;
+      deprecate("ignoreNotFound is deprecated", "Please use stubNotFound");
     }
 
-    return context;
-  });
-};
+    configureNotifications(this, this.options.notifications);
+    configureLogger(this, this.options.log, loggerFactory);
+  }
 
-Bitbundler.prototype.update = function(files) {
-  var file = new File(files);
-  var bitbundler = this;
-  var context = bitbundler.context;
-  logger.log("build-start");
+  bundle(files) {
+    var file = new File(files);
+    this.context = this._createContext(file, this.options);
+    logger.log("build-init");
 
-  file.src
-    .filter(function(filePath) {
-      return context.cache[filePath];
-    })
-    .forEach(function(filePath) {
-      context.loader.deleteModule(context.cache[filePath]);
-    });
+    return this.update(files).then((context) => {
+      if (this.options.watch) {
+        watch(this, this.options.watch);
 
-  return bitbundler.context.execute(file.src)
-    .then(function writeBundle(context) {
-      logger.log("build-writing");
-      return bitbundler._writeContext(context);
-    })
-    .then(function(context) {
-      bitbundler.context = context;
-
-      context.visitBundles(function(bundle) {
-        logger.log("bundle", bundle);
-      });
-
-      logger.log("build-success", context);
-      logger.log("build-end");
-      return context;
-    })
-    .catch(function(err) {
-      if (err) {
-        process.stderr.write((err && err.stack ? err.stack : err) + "\n");
+        if (this.options.multiprocess) {
+          context.loader.pool.size(1);
+        }
       }
-
-      if (bitbundler.options.multiprocess) {
+      else if (this.options.multiprocess) {
         context.loader.pool.stop();
       }
 
-      logger.error("build-failure", err);
-      logger.log("build-end");
-      throw err;
+      this.context = context;
+      return context;
     });
-};
+  }
 
-Bitbundler.prototype.hasModule = function(modulePath) {
-  return this.context.cache.hasOwnProperty(modulePath);
-};
+  update(files) {
+    var file = new File(files);
+    logger.log("build-start");
 
-Bitbundler.prototype.getModules = function() {
-  return this.context.cache;
-};
+    file.src
+      .filter((filePath) => this.context.cache[filePath])
+      .forEach((filePath) => this.context.loader.deleteModule(this.context.cache[filePath]));
 
-Bitbundler.prototype.getLogger = function(name) {
-  return loggerFactory.create(name);
-};
+    return this.context
+      .execute(file.src)
+      .then((context) => {
+        logger.log("build-writing");
+        return this._writeContext(context);
+      })
+      .then((context) => {
+        context.visitBundles((bundle) => logger.log("bundle", bundle));
+        logger.log("build-success", context);
+        logger.log("build-end");
+        this.context = context;
+        return context;
+      })
+      .catch((err) => {
+        if (err) {
+          process.stderr.write((err && err.stack ? err.stack : err) + "\n");
+        }
+
+        if (this.options.multiprocess) {
+          context.loader.pool.stop();
+        }
+
+        logger.error("build-failure", err);
+        logger.log("build-end");
+        throw err;
+      });
+  }
+
+  hasModule(modulePath) {
+    return this.context.cache.hasOwnProperty(modulePath);
+  }
+
+  getModules() {
+    return this.context.cache;
+  }
+
+  getLogger(name) {
+    return loggerFactory.create(name);
+  }
+
+  _createContext(file, options) {
+    return new Context({
+      file: file,
+      loader: createLoader(options),
+      bundler: createBundler(options)
+    });
+  }
+
+  _writeContext(context) {
+    return bundleWriter()(context);
+  }
+}
 
 Bitbundler.bundle = function(files, settings) {
   return new Bitbundler(settings).bundle(files);
 };
 
-Bitbundler.prototype._createContext = function(file, options) {
-  return new Context({
-    file: file,
-    loader: createLoader(options),
-    bundler: createBundler(options)
-  });
-};
-
-Bitbundler.prototype._writeContext = function(context) {
-  return bundleWriter()(context);
-};
 
 function createLoader(options) {
   if (Array.isArray(options.loader)) {
@@ -162,13 +151,13 @@ function configureNotifications(bitbundler, notifications) {
     return;
   }
 
-  [].concat(notifications).forEach(function(notification) {
+  [].concat(notifications).forEach((notification) => {
     if (types.isFunction(notification)) {
       notification = notification(bitbundler) || {};
     }
 
-    Object.keys(notification).forEach(function(notificationName) {
-      [].concat(notification[notificationName]).forEach(function(cb) {
+    Object.keys(notification).forEach((notificationName) => {
+      [].concat(notification[notificationName]).forEach((cb) => {
         bitbundler.on(notificationName, cb);
       });
     });
