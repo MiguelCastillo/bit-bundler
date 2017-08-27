742ef8db8653b835aa8aa928e40869681811c645
Miguel Castillo <mcastillo@symphono.com>
2017-08-26 22:24:52 -0400

    adding example webapp
    with support for:
    - dev server with live-server
    - copying of static resources with cpx
    - bundling with bit-bundler
    - process management with pm2

---
+++ examples/webapp/dev.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  apps : [{
+    name: "assets",
+    script: "npm",
+    args: "run assets -- --watch",
+    watch: false
+  }, {
+    name: "server",
+    script: "npm",
+    args: "run server",
+    watch: [".live-server.js"],
+    ignore_watch: ["node_modules"]
+  }, {
+    name: "build",
+    script: "npm",
+    args: "run build -- --watch",
+    watch: [".bitbundler.js"],
+    ignore_watch: ["node_modules"],
+    env: {
+      "NODE_ENV": "development",
+    },
+    env_production : {
+      "NODE_ENV": "production"
+    }
+  }]
+};
