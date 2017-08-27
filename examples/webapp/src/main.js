3e33d28b8d50a0b2ff5baa5b8c0b2bb353474309
Miguel Castillo <mcastillo@symphono.com>
2017-08-27 11:47:12 -0400

    converted hello.js to be a react component that says "Hello world"

--- examples/webapp/src/main.js
+++ examples/webapp/src/main.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Hello from "./hello";
 
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <h1><Hello></Hello></h1>,
   document.getElementById("root")
 );
