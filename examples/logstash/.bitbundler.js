53a1bcb6c6c2ada3b6fc4a9277c86a3f46e3cddd
Miguel Castillo <manchagnu@gmail.com>
2017-11-29 14:46:26 -0500

    Adjust the bit-bundler config file to pipe data to a TCP connection logstash is listening on

--- examples/logstash/.bitbundler.js
+++ examples/logstash/.bitbundler.js
@@ -1,9 +1,19 @@
-var loggers = require("bit-bundler/loggers");
-var loaderFilter = require("bit-bundler/loggers/loaderFilter");
-var JSONStream = require("JSONStream");
+const loggers = require("bit-bundler/loggers");
+const loaderFilter = require("bit-bundler/loggers/loaderFilter");
+const JSONStream = require("JSONStream");
+const net = require("net");
+
+// TCP client to write to logstash with.
+const client = net.createConnection({ port: 9400 });
 
 module.exports = {
-  log: loggers.sequence(loaderFilter(), JSONStream.stringify(false), process.stdout),
+  log: loggers.sequence(loaderFilter(), JSONStream.stringify(false), client),
   src: "src/main.js",
-  dest: "dist/out.js"
+  dest: "dist/out.js",
+
+  notifications: [{
+    "build-end": () => {
+      client.end();
+    }
+  }]
 };
