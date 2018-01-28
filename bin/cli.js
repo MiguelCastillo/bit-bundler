993b5c7cee16bd1d95770640852e7b4262a9bfa3
Miguel Castillo <manchagnu@gmail.com>
2018-01-27 23:01:28 -0500

    adding implementation for piping data into bitbundler via the process.stdin stream

--- bin/cli.js
+++ bin/cli.js
@@ -2,6 +2,7 @@
 
 /*eslint no-console: ["off"]*/
 
+const path = require("path");
 const utils = require("belty");
 const options = require("../src/options")(process.argv.slice(2));
 const files = utils.pick(options, ["src", "dest", "content", "path"]);
@@ -11,5 +12,38 @@ if (options.print) {
   console.log("options: ", JSON.stringify(options));
 }
 else {
-  require("../src/index").bundle(files, utils.omit(options, ["src", "dest", "content", "path"]));
+  const Bitbundler = require("../src/index");
+  const settings = utils.omit(options, ["src", "dest", "content", "path"]);
+
+  if (files.src) {
+    Bitbundler.bundle(files, settings);
+  }
+  else {
+    process.stdout.write("> waiting for stream input...\n");
+    readFromStdin((content) => {
+      Bitbundler.bundle({
+        src: [{
+          source: content,
+          path: path.join(process.cwd(), (files.path || "/$anonymous.js"))
+        }],
+        dest: files.dest
+      }, settings);
+    });
+  }
+}
+
+function readFromStdin(cb) {
+  var source = "";
+
+  process.stdin
+    .setEncoding("utf8")
+    .on("readable", () => {
+      var chunk;
+      while ((chunk = process.stdin.read())) {
+        source += chunk;
+      }
+    })
+    .on("end", () => {
+      cb(source);
+    });
 }
